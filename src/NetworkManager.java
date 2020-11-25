8f2f36f4814d49170ca7cad9ea661ccca463c860
Emma Baudoint <62210726+Emma-bau@users.noreply.github.com>
2020-11-25 09:07:39 +0100

    network waiter update

--- src/NetworkManager.java
+++ src/NetworkManager.java
@@ -1,4 +1,26 @@
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class NetworkManager {
+
+	public void server() 
+	{
+		try {
+			/*Création de notre serveur locale d'écoute*/
+			ServerSocket serv = new ServerSocket(1234);
+			/*On se met en écoute tant que la session est ouverte*/
+			while(true)
+			{
+				/*Attente de connexion*/
+				System.out.println("Awaiting connection");
+				Socket SocketTCP = serv.accept();
+
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
