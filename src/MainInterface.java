3e4092ddb248fd68dfc750d72cc5d43d3a89f1a2
Matthieu Jacques <74052376+Matthieu-Jck@users.noreply.github.com>
2020-12-01 16:57:12 +0100

    Update MainInterface.java

--- src/MainInterface.java
+++ src/MainInterface.java
@@ -12,43 +12,39 @@ public class MainInterface extends JFrame implements ActionListener{
 		JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Chat Session");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
-        // Define new buttons with different width on help of the ---
-        JButton jb1 = new JButton("Button 1 -");        
-        JButton jb2 = new JButton("Button 2 --------");
-        JButton jb3 = new JButton("Button 3 -");        
-        JButton jb4 = new JButton("Button 4 --------");
+        toScreenSize(frame);
+        layout(frame);
+        toScreenSize(frame);
+        
+	}
+    
+    public void layout(JFrame frame) {
+        JLabel welcomeText = new JLabel();
+		welcomeText.setText("Hello "+ agent.getPseudoManager().getPseudo() + "!");
+        float fontSize = 40;
+        welcomeText.setFont(welcomeText.getFont().deriveFont(fontSize));
+		      
         JButton jb5 = new JButton("Button 5 -");        
         JButton jb6 = new JButton("Button 6 --------");
          
         // Define the panel to hold the buttons 
         JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
         JPanel panel3 = new JPanel();
          
         // Set up the title for different panels
-        panel1.setBorder(BorderFactory.createTitledBorder("LEFT"));
-        panel2.setBorder(BorderFactory.createTitledBorder("CENTER"));
+        panel1.setBorder(BorderFactory.createTitledBorder("Infos"));
         panel3.setBorder(BorderFactory.createTitledBorder("RIGHT"));
          
+        
         // Set up the BoxLayout
         BoxLayout layout1 = new BoxLayout(panel1, BoxLayout.Y_AXIS);
-        BoxLayout layout2 = new BoxLayout(panel2, BoxLayout.Y_AXIS);
         BoxLayout layout3 = new BoxLayout(panel3, BoxLayout.Y_AXIS);
+        panel1.add(welcomeText);
         panel1.setLayout(layout1);
-        panel2.setLayout(layout2);
         panel3.setLayout(layout3);
-         
-        // Add the buttons into the panel with three different alignment options
-        jb1.setAlignmentX(Component.LEFT_ALIGNMENT);
-        jb2.setAlignmentX(Component.LEFT_ALIGNMENT);
-        panel1.add(jb1);
-        panel1.add(jb2);
-         
-        jb3.setAlignmentX(Component.CENTER_ALIGNMENT);
-        jb4.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel2.add(jb3);
-        panel2.add(jb4);
+
+        
+
          
         jb5.setAlignmentX(Component.RIGHT_ALIGNMENT);
         jb6.setAlignmentX(Component.RIGHT_ALIGNMENT);
@@ -56,16 +52,16 @@ public class MainInterface extends JFrame implements ActionListener{
         panel3.add(jb6);
          
         // Add the three panels into the frame
-        frame.setLayout(new FlowLayout());
+        frame.setLayout(new GridLayout());
         frame.add(panel1);
-        frame.add(panel2);
         frame.add(panel3);
-        
+       
         frame.pack();
         frame.setVisible(true);
-        toScreenSize(frame);
-	}
-    
+
+
+    	
+    }
     public void toScreenSize(JFrame frame) {
     	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     	int screenHeight = screenSize.height;
