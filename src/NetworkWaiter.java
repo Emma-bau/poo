19aaf19360da9ab2a21c0bce45f9dc37119b86ed
Emma Baudoint <emma.baudoint@laposte.net>
2020-12-16 11:29:05 +0100

    update

--- src/NetworkWaiter.java
+++ src/NetworkWaiter.java
@@ -9,42 +9,32 @@ public class NetworkWaiter implements Runnable {
 	private final Socket link;
 	private final NetworkManager networkManager;
 	private final InetAddress adresse;
+	private PrintWriter out;
+	private BufferedReader in;
+
 
 	public NetworkWaiter(Socket link, NetworkManager networkManager, InetAddress adresse) {
 		this.link = link;
 		this.networkManager = networkManager;
 		this.adresse=adresse;
+		try{
+			out = new PrintWriter(link.getOutputStream(),true);
+			in = new BufferedReader (new InputStreamReader (link.getInputStream()));
+		}
+		catch(IOException e)
+		{
+			System.out.println("Erreur création des bufffers, serveur tcp");
+		}
 	}
 
-	/* On gere la connexion de nos nouveaux utilisateurs dans un thread */
-	/*Reste a gerer avec l'id a qui on envoit*/
+
+
 	@Override
 	public void run()
 	{
 		System.out.println("Thread du serveur lance");
 		try 
 		{
-			PrintWriter out = new PrintWriter(link.getOutputStream(),true);
-			BufferedReader in = new BufferedReader (new InputStreamReader (link.getInputStream()));
-			/*Mise en place d'un thread pour l'envoie des messages*/
-			Thread envoie = new Thread(new Runnable() {
-				String msg;
-				@Override
-				public void run() {
-					while(true)
-					{
-							//On envoie le message si l'adresse correspond à l'adresse de notre client
-							if ((networkManager.getSendMessage() != null ) && (networkManager.getSendMessage().getContact().getAdresse()==adresse))
-							{
-								msg = networkManager.getSendMessage().getMessage();
-								out.println(msg);
-								out.flush();
-							}
-
-					}
-				}
-			});
-			envoie.start();
 
 			/*Mise en place d'un thread pour recevoir les messages*/
 			Thread recevoir= new Thread(new Runnable() {
@@ -89,6 +79,16 @@ public class NetworkWaiter implements Runnable {
 		System.out.println("Erreur au niveau du serveur niveau 2");
 	}
 
-}
+	
+
+
+	}
+	public void envoie (Message message)
+	{
+		String msg;
+		msg = message.getMessage();
+		out.println(msg);
+	}
+
 
 }
