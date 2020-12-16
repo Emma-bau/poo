380a8cd6b83624ced160aec5d8790c80777cad82
Emma Baudoint <emma.baudoint@laposte.net>
2020-12-16 11:35:29 +0100

    networkwaiter

--- src/NetworkWaiter.java
+++ src/NetworkWaiter.java
@@ -1,6 +1,5 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
 
 /*Serveur en attente d'une connexion de quelqu'un a notre client */
 
@@ -8,15 +7,14 @@ public class NetworkWaiter implements Runnable {
 
 	private final Socket link;
 	private final NetworkManager networkManager;
-	private final InetAddress adresse;
 	private PrintWriter out;
 	private BufferedReader in;
+	private Contact user;
 
 
 	public NetworkWaiter(Socket link, NetworkManager networkManager, InetAddress adresse) {
 		this.link = link;
 		this.networkManager = networkManager;
-		this.adresse=adresse;
 		try{
 			out = new PrintWriter(link.getOutputStream(),true);
 			in = new BufferedReader (new InputStreamReader (link.getInputStream()));
@@ -25,6 +23,16 @@ public class NetworkWaiter implements Runnable {
 		{
 			System.out.println("Erreur création des bufffers, serveur tcp");
 		}
+
+		/*On récupère le contact associé à l'adresse*/
+		for (Contact C : networkManager.getconnectedUser())
+		{
+			if (C.getAdresse() == adresse)
+			{
+				user = C;
+			}
+		}
+
 	}
 
 
@@ -35,54 +43,41 @@ public class NetworkWaiter implements Runnable {
 		System.out.println("Thread du serveur lance");
 		try 
 		{
-
-			/*Mise en place d'un thread pour recevoir les messages*/
-			Thread recevoir= new Thread(new Runnable() {
-				String msg ;
-				Contact client;
-				@Override
-				public void run() {
-					try {
-						msg = in.readLine();
-						//tant que le client est connecte
-						while(msg!=null)
-						{
-							msg = in.readLine();
-							//revoir id source ? 
-							for (Contact C : networkManager.getconnectedUser())
-							{
-								if (C.getAdresse() == adresse)
-								{
-									client = C;
-								}
-							}
-							Message m = new Message(msg,java.time.LocalDate.now(),client);
-							networkManager.setReceiveMessage(m);
-						}
-						//sortir de la boucle si le client a deconecte
-						System.out.println("Client serveur deconecte");
-						//fermer le flux et la session socket
-						out.close();
-						link.close();
-						} 
-					catch (IOException e) {
-							System.out.println("probleme dans recevoir");
-						}
+			String msg ;
+			try {
+				msg = in.readLine();
+				//tant que le client est connecte
+				while(msg!=null)
+				{
+					msg = in.readLine();
+					System.out.println(msg);
+					Message m = new Message(msg,java.time.LocalDate.now(),user);
+					networkManager.setReceiveMessage(m);
 				}
-			});
-		recevoir.start();
-				
-	}
+				//sortir de la boucle si le client a deconecte
+				System.out.println("Client serveur deconecte");
+				//fermer le flux et la session socket
+				out.close();
+				link.close();
+				} 
+			catch (IOException e) 
+			{
+				System.out.println("probleme dans recevoir");
+			}
+			
 
-	catch(Exception e)
-	{
-		System.out.println("Erreur au niveau du serveur niveau 2");
-	}
+		}
+
+		catch(Exception e)
+		{
+			System.out.println("Erreur au niveau du serveur niveau 2");
+		}
 
 	
 
 
 	}
+
 	public void envoie (Message message)
 	{
 		String msg;
