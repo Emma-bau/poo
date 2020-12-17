4bfc4f4ed9b372703a8008a533da2cf9d50f74b5
Emma Baudoint <emma.baudoint@laposte.net>
2020-12-17 10:31:11 +0100

    data

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 import java.time.LocalDate;
+import java.io.IOError;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DataManager {
 
@@ -19,6 +27,43 @@ public class DataManager {
 		Message m = new Message(texte, LocalDate.now(),contact);
 		
 		// BDD
+		/*Rajouter hier soir à voir comment ça fonctionne */
+		try
+		{
+			Connection con = null;
+			/*Je ne suis pas sure du nom*/
+			/*On load le driver*/
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+			/*Mise en place de la connexion*/
+			try
+			{
+				con=DriverManager.getConnection("jdbc:odbc:MovieCatalog");
+				Statement statement=con.createStatement();
+
+				
+				//ResultSet rs=statement.executeQuery("SELECT*"+FROMtable);
+				/*On ferme la BDD*/
+				con.close();
+
+			}
+			catch(SQLException e)
+			{
+				System.err.println(e);
+			}
+			
+		}
+		catch (ClassNotFoundException e)
+		{
+			System.out.println("Erreur dans la creation de la BDD");
+		}
+
+
+
+
+
+
+
+
 		this.messagesHistory.add(m);
 		//
 		
